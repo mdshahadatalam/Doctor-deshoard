857ca916f8d7229c2ccbdb3f2dedf52abb5828d4
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-02-11 22:07:52 -0800

    all

--- src/Component/AddDoctor.jsx
+++ src/Component/AddDoctor.jsx
@@ -1,68 +1,173 @@
 import React, { useState } from 'react'
 import './AddDoctor.css'
+import axios from 'axios'
 
 export const AddDoctor = () => {
 const [name,setName] = useState("")
 const [email,setEmail] = useState("")
+const [password,setPassword] = useState("")
+const [expe,setExpe] = useState("")
+const [fees,setFess] = useState("")
+const [about,setAbout] = useState("")
+const [spe,setSpe] = useState("")
+const [address,setAddress] = useState("")
+const [addressTwo,setAddressTwo] = useState("")
 
-  // const handleSubmit =()=>{
-  //   console.log(name,email)
-  // }
+  const handleSubmit =()=>{
+    console.log(name,email,password,expe,fees,about,spe,address,addressTwo);
+
+    axios.post('http://localhost:3000/addDoctor',{
+      name:name,
+      email:email,
+      password:password,
+      experience:expe,
+      fess:fees,
+      about:about,
+      spe:spe,
+      addressOne:address,
+      addressTwo:addressTwo,
+    }).then(res=>{
+      console.log(res);
+      setName('')
+      setEmail('')
+      setPassword('')
+      setExpe('')
+      setFess('')
+      setAbout('')
+      setSpe('')
+      setAddress('')
+      setAddressTwo('')
+    }).catch(err=>{
+      console.log(err);
+    })
+    
+  }
   return (
     <>
       <div className="row">
           <div className="col-lg-6  col-md-6 ps-4">
            <div className=''>
            <label className='lableText' htmlFor="">Doctor name</label> <br />
-           <input className='inpuField' type="text" name="name" id="name" placeholder='Name'/>
+           <input
+            className='inpuField'
+             type="text"
+              name="name" 
+              id="name"
+               placeholder='Name'
+               onChange={(e)=>{setName(e.target.value)}}
+               value={name}
+               />
            </div>
 
              <div className='mt-3'>
              <label className='lableText' htmlFor="">Doctor Email</label> <br />
-             <input className='inpuField' type="email" name="email" id="email" placeholder='Email'/>
+             <input
+              className='inpuField' 
+              type="email"
+               name="email"
+                id="email" 
+                placeholder='Email'
+                onChange={(e)=>{setEmail(e.target.value)}}
+                value={email}
+                />
              </div>
 
 
             <div className='mt-3'>
             <label className='lableText' htmlFor="">Doctor Password</label> <br />
-            <input className='inpuField' type="password" name="password" id="password" placeholder='New password'/>
+            <input 
+            className='inpuField'
+             type="password"
+              name="password" 
+              id="password"
+               placeholder='New password'
+               onChange={(e)=>{setPassword(e.target.value)}}
+               value={password}
+               />
             </div>
 
            <div className='mt-3'>
            <label className='lableText' htmlFor="">Experience</label> <br />
-           <input className='inpuField' type="text" name="Experience" id="Experience"  placeholder='Experience'/>
+           <input
+            className='inpuField'
+             type="text"
+              name="Experience"
+               id="Experience"
+                 placeholder='Experience'
+                 onChange={(e)=>{setExpe(e.target.value)}}
+                 value={expe}
+                 />
            </div>
 
 
             <div className='mt-3'>
             <label className='lableText' htmlFor="">Fees</label> <br />
-            <input className='inpuField' type="text" name="fees" id="fees" placeholder='Your fees'/>
+            <input
+             className='inpuField'
+             type="text" 
+             name="fees" 
+             id="fees" 
+             placeholder='Your fees'
+             onChange={(e)=>{setFess(e.target.value)}}
+             value={fees}
+             />
             </div>
 
             <div className='my-3'>
                <label className='lableText' htmlFor="">About me</label> <br/>
-               <textarea className='message' name="message" id="message" placeholder='Message'></textarea>
+               <textarea 
+               className='message'
+               name="message" 
+               id="message"
+               placeholder='Message'
+               onChange={(e)=>{setAbout(e.target.value)}}
+               value={about}
+               ></textarea>
             </div>
 
           </div>
           <div className="col-lg-6 col-md-6">
           <div className=''>
            <label className='lableText' htmlFor="">Speciality</label> <br />
-           <input className='inpuField' type="text" name="name" id="name" placeholder='Speciality'/>
+           <input 
+           className='inpuField'
+            type="text"
+             name="Speciality" 
+             id="Speciality" 
+             placeholder='Speciality'
+             onChange={(e)=>{setSpe(e.target.value)}}
+             value={spe}
+             />
            </div>
 
              <div className='mt-3'>
              <label className='lableText' htmlFor="">Address</label> <br />
-             <input className='inpuField' type="text" name="Address1" id="Address1" placeholder='Address 1'/>
+             <input
+              className='inpuField'
+               type="text"
+                name="Address1" 
+                id="Address1" 
+                placeholder='Address 1'
+                onChange={(e)=>{setAddress(e.target.value)}}
+                value={address}
+                />
              </div>
 
 
             <div className='mt-3'>
-            <input className='inpuField' type="text" name="Address2" id="Address2" placeholder='Address 2'/>
+            <input
+             className='inpuField'
+              type="text" 
+              name="Address2" 
+              id="Address2" 
+              placeholder='Address 2'
+              onChange={(e)=>{setAddressTwo(e.target.value)}}
+              value={addressTwo}
+              />
             </div>
           </div>
 
-          {/* <button onClick={handleSubmit}>submit</button> */}
+          <button onClick={handleSubmit} className='addDoc'>Add doctor</button>
       </div>
     </>
   )
